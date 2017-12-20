5f7872ee255ab9d621e7970a62be4197b520fadd
Calum-W <cal7907@hotmail.co.uk>
2017-12-20 14:01:47 +0000

    Replaces all plane instances in airport spec with spies

--- jasmine/spec/AirportSpec.js
+++ jasmine/spec/AirportSpec.js
@@ -1,17 +1,14 @@
 describe("Airport", function() {
 
-  var plane;
-  var plane2;
   var spyPlane
   var airport;
   var clearWeather;
 
   beforeEach(function() {
-    plane = new Plane();
-    plane2 = new Plane();
     airport = new Airport();
     clearWeather = new Weather();
     spyPlane = jasmine.createSpyObj('spyPlane', ['takeOff', 'land']);
+    spyPlane2 = jasmine.createSpyObj('spyPlane2', ['takeOff', 'land']);
     spyOn(clearWeather, 'isStormy').and.returnValue(false);
   });
 
@@ -26,45 +23,45 @@ describe("Airport", function() {
 
   describe("land", function() {
     it("should add a plane to the hangar", function() {
-      airport.land(plane, clearWeather);
-      expect(airport._hangar).toContain(plane);
+      airport.land(spyPlane, clearWeather);
+      expect(airport._hangar).toContain(spyPlane);
     });
 
     it("should throw an error if the airport is already at capacity", function() {
       for (var i = 0; i < airport.CAPACITY; i++) {
         airport.land(new Plane(), clearWeather);
       }
-      expect(function() {airport.land(plane, clearWeather)}).toThrow(new Error('Airport full'));
+      expect(function() {airport.land(spyPlane, clearWeather)}).toThrow(new Error('Airport full'));
     });
 
     it("should tell the plane to land", function() {
-      airport.land(spyPlane);
+      airport.land(spyPlane, clearWeather);
       expect(spyPlane.land).toHaveBeenCalled();
     });
   });
 
   describe("take off", function() {
     it("should remove a plane from the hangar", function() {
-      airport.land(plane, clearWeather);
-      airport.takeOff(plane, clearWeather);
+      airport.land(spyPlane, clearWeather);
+      airport.takeOff(spyPlane, clearWeather);
       expect(airport._hangar.length).toEqual(0);
     });
 
     it("should only remove the correct plane from the hangar", function() {
-      airport.land(plane, clearWeather);
-      airport.land(plane2, clearWeather);
-      airport.takeOff(plane, clearWeather);
-      expect(airport._hangar[0]).toEqual(plane2);
+      airport.land(spyPlane, clearWeather);
+      airport.land(spyPlane2, clearWeather);
+      airport.takeOff(spyPlane, clearWeather);
+      expect(airport._hangar[0]).toEqual(spyPlane2);
     });
 
     it("should throw an error if the plane is not docked at that airport", function() {
-      plane.isFlying = false
-      expect(function() {airport.takeOff(plane)}).toThrow(new Error('Plane not docked at this airport'));
+      spyPlane.isFlying = false
+      expect(function() {airport.takeOff(spyPlane, clearWeather)}).toThrow(new Error('Plane not docked at this airport'));
     });
 
     it("should tell the plane to take off", function() {
       airport.land(spyPlane);
-      airport.takeOff(spyPlane);
+      airport.takeOff(spyPlane, clearWeather);
       expect(spyPlane.takeOff).toHaveBeenCalled();
     });
   });
@@ -72,26 +69,26 @@ describe("Airport", function() {
 
 describe("stormy weather", function() {
 
-  var plane;
+  var spyPlane;
   var airport;
   var stormyWeather;
 
   beforeEach(function() {
-    plane = new Plane();
+    spyPlane = jasmine.createSpyObj('spyPlane', ['takeOff', 'land']);
     airport = new Airport();
     stormyWeather = new Weather
     spyOn(stormyWeather, 'isStormy').and.returnValue(true);
   });
 
   it("blocks take off when the weather is stormy", function() {
-    airport._hangar.push(plane);
-    plane.isFlying = false;
-    expect(function(){ airport.takeOff(plane, stormyWeather) }).toThrowError('Stormy')
-    expect(airport._hangar).toContain(plane);
+    airport._hangar.push(spyPlane);
+    spyPlane.isFlying = false;
+    expect(function(){ airport.takeOff(spyPlane, stormyWeather) }).toThrowError('Stormy')
+    expect(airport._hangar).toContain(spyPlane);
   });
 
   it("blocks land when the weather is stormy", function() {
-    expect(function(){ airport.land(plane, stormyWeather) }).toThrowError('Stormy')
+    expect(function(){ airport.land(spyPlane, stormyWeather) }).toThrowError('Stormy')
     expect(airport._hangar.length).toEqual(0);
   });
 });
